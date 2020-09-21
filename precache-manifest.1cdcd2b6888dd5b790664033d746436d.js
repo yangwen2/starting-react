cd18fa9d3300b21e81a64e6313d987c6a3d05334
Yang Wen <yangwen2@gmail.com>
2020-09-21 07:06:56 -0600

    Updates

---
+++ precache-manifest.1cdcd2b6888dd5b790664033d746436d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b7d409f041a004185ba19829900d24",
+    "url": "/starting-react/index.html"
+  },
+  {
+    "revision": "cc405bf4393c26efa28f",
+    "url": "/starting-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5fd1581e8b8a411ad2e1",
+    "url": "/starting-react/static/js/2.64ab2b37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/starting-react/static/js/2.64ab2b37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc405bf4393c26efa28f",
+    "url": "/starting-react/static/js/main.5eeac23d.chunk.js"
+  },
+  {
+    "revision": "cbe0223f76f3889817ae",
+    "url": "/starting-react/static/js/runtime-main.5bc147b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/starting-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
